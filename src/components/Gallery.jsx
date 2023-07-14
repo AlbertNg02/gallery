69257b56ddee63836c007b938e4102ab3a332e43
Albert_Nguyen <77342590+AlbertNg02@users.noreply.github.com>
2023-07-14 14:35:10 +0700

    Updated functionaility

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,64 +1,33 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './Gallery.css';
 
 
 
-import Img1 from '../uploads/img_1.jpg';
-import Img2 from '../uploads/img_2.jpg';
-import Img3 from '../uploads/img_3.jpg';
-import Img4 from '../uploads/img_4.jpg';
-import Img5 from '../uploads/img_5.jpg';
-import Img6 from '../uploads/img_6.jpg';
 
 
 const Gallery = () => {
 
-    let data = [
-        {
-            id: 1,
-            imgSrc: Img1,
-        },
-        {
-            id: 2,
-            imgSrc: Img2,
-        },
-        {
-            id: 3,
-            imgSrc: Img3,
-        },
-        {
-            id: 4,
-            imgSrc: Img4,
-        },
-        {
-            id: 5,
-            imgSrc: Img5,
-        },
-        {
-            id: 6,
-            imgSrc: Img6,
-        },
 
-    ]
-
-    const [model, setModel] = useState(false);
-    const [tempingSrc, setTempImgSrc] = useState('');
-    const getIMg = (imgSrc) => {
-        setTempImgSrc(imgSrc);
-        setModel(true);
+    function importAll(r) {
+        // console.log("inside importAll")`
+        let images = {};
+        r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+        return images;
     }
+
+    const data = importAll(require.context('../uploads', true, /\.(jpg|jpeg)$/));
+    console.log(data);
+
 
     return (
         <>
-            <div className={model ? "model open" : "model"}><img onScroll={() => setModel(false)}></img>
-            </div>
+
 
             <div className='gallery'>
-                {data.map((item, index) => {
+                {Object.entries(data).map(([key, value], index) => {
                     return (
-                        <div className='pics' key={index}
-                            onClick={() => getIMg(item.imgSrc)}>
-                            <img src={item.imgSrc} style={{ width: '100%',   }} />
+                        <div className='pics' key={index}>
+                            <img src={value} style={{ width: '100%', }} />
                         </div>
                     )
                 })}
